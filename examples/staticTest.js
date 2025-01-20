96f35cec92e4f75e84101ab6e5ea50cfa3b32c22
GenerelSchwerz <xXSG.KingXx@gmail.com>
2025-01-20 18:22:55 -0500

    enhance static test showcase

--- examples/staticTest.js
+++ examples/staticTest.js
@@ -1,14 +1,24 @@
 const mcVersion = "1.18.2"
 
+function stringifyItem(registry, item) {
+  const mdItem = registry.items[item.id]
+  return `${mdItem.name} x ${item.count}`
+}
+
+function beautifyPlan(registry, plan) {
+  const items = plan.itemsRequired.filter(i=>i.count>0).map(stringifyItem.bind(null,registry)).join(", ")
+  return items
+}
+
 async function main(mcVersion) {
 
   const mcData = require("minecraft-data")(mcVersion)
   const crafter = await require("mineflayer-crafting-util").buildStatic(mcVersion) // buildStatic is async
 
-  const sticks = {id: mcData.itemsByName.stick.id, count: 1}
+  const sticks = {id: mcData.itemsByName.stone_sword.id, count: 1}
   const plan = crafter(sticks)
-  console.log(plan)
 
+  console.log(beautifyPlan(mcData, plan))
 }
 
 (async () => main(mcVersion))()
